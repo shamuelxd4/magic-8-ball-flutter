924caa4256e0a765952e132ae42b798705459242
Samuel Quenta Carvajal <50058913+shamuelxd4@users.noreply.github.com>
2021-02-26 23:01:17 -0400

    realizando el chalens de la bola 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Home(),
       ),
     );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade800,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey.shade900,
+        title: Center(
+          child: Text(
+            "Ball 8",
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+      ),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int numBola = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: FlatButton(
+            padding: EdgeInsets.all(0),
+            onPressed: () {
+              setState(() {
+                numBola = (Random().nextInt(5)) + 1;
+              });
+            },
+            child: Image.asset('images/ball$numBola.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
